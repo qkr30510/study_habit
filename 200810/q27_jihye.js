a8797cef638533f25ae1578ad0c7bb14c2875649
kimji9663 <rlawlgp9663@naver.com>
2020-08-10 23:49:48 +0900

    27. 객체 만들기

--- 200810/q27_jihye.js
+++ 200810/q27_jihye.js
@@ -12,3 +12,12 @@
 // 출력
 // {'Yujin': 70, 'Hyewon': 100}
 
+const keys = prompt('이름을 입력하세요').split(' ');
+const values = prompt('점수를 입력하세요').split(' ');
+const obj = {};
+
+for (let i=0; i<keys.length; i++) {
+  obj[keys[i]] = parseInt(values[i], 10);
+}
+
+console.log(obj);
