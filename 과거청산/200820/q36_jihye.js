bea9f1d7a73e2fc34b52e09a1d2704cdcad649bf
kimji9663 <rlawlgp9663@naver.com>
2020-08-27 22:44:06 +0900

    36번 다시 풀어보기

--- 과거청산/200820/q36_jihye.js
+++ 과거청산/200820/q36_jihye.js
@@ -11,17 +11,49 @@
 // 출력
 // 혜원(이)가 총 4표로 반장이 되었습니다.
 
+
+// 입력
+// 원범 원범 혜원 혜원 혜원 혜원 유진 유진
+
 const array = prompt('이름을 입력해주세요.').split(' ');
-let result = {};
+//let array = '원범 원범 혜원 혜원 혜원 혜원 유진 유진';
+array = array.split(' ');
+let result = {};//객체 선언
+/*
+객체는 {} 중괄호를 이용해 만들며, 키(key):값(value) 쌍으로 구성된 property를 여러개 넣을 수 있다.
+키는 문자형, 값은 모든 자료형이 허용된다. 
+*/
 let winner = "";
 
+console.log('result : ' + result);
+
 for(let index in array){
-  let val = array[index];
+  let val = array[index]; 
+  console.log('val :' + val);
   result[val] = result[val] === undefined ? 1 : result[val] = result[val] + 1;
+  /*
+  result[val] 초기값은 undefined.
+  result[val]의 값이 undefined이면 1을 담는다. => result[val]의 값이 1이 됨.
+  result[val]의 값이 undefined가 아니면 현재 값에 1을 더한다.
+  */
+  console.log('result[val] : ' + result[val]);
+  //console.log('개표 : ' + array[index], result[val]);
 }
+
+
+console.log('결과 : ' + Object.keys(result));
+
 
 winner = Object.keys(result).reduce(function(a, b){
   return result[a] > result[b] ? a : b
+  /*
+  .reduce()는 배열의 각 요소에 리듀서 함수를 실행하고, 하나의 결과값을 반환한다.
+  배열을 순회하며 인덱스 데이터를 줄여가면서 기능을 수행함.
+  배열 a, b, c가 있을때,
+  a와 b를 비교하여 하나의 값을 반환, 반환된 값을 c와 비교하여 다시 하나의 값을 반환.
+  */
 });
+console.log('결과 : ' + winner);
+
 
 console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
