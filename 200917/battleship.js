3725b5cdfd19bbfedb7a8c816039ddfe4c548a96
kimji9663 <rlawlgp9663@naver.com>
2020-09-17 09:52:57 +0900

    Headfirst - 2. Battleship (Default)

---
+++ 200917/battleship.js
@@ -0,0 +1,30 @@
+// Headfirst JS - 2. Battleship
+
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+// 전함이 가라않지 않은 동안 게이머가 추측한 값 입력
+
+//준비, 조준, 발사! (0 ~ 6사이 숫자입력)
+
+//0보다 작거나 6보다 크면,(guess값이 0 ~ 6 사이에 있는지 확인)
+
+//올바른 방 번호를 입력하세요!
+
+//추측한 횟수에 1을 더한다.
+        
+//1 또는 2, 3번과 일치하면
+
+//hit 증가.
+
+//전함 침몰!
+
+//실패
+
+//n번 발사! 명중률 : n%
+
