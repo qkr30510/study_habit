2d4a2a4ce3e25b6d21f54a8a420dd20293b0ddc5
kimji9663 <rlawlgp9663@naver.com>
2020-09-17 10:23:03 +0900

    Headfirst - 2. Battleship

--- 200917/battleship.js
+++ 200917/battleship.js
@@ -9,22 +9,41 @@ var guesses = 0;
 var isSunk = false;
 
 // 전함이 가라않지 않은 동안 게이머가 추측한 값 입력
+while (isSunk === false){
+    //준비, 조준, 발사! (0 ~ 6사이 숫자입력)
+    guess = prompt('준비, 조준, 발사! (0 ~ 6사이 숫자입력)');
 
-//준비, 조준, 발사! (0 ~ 6사이 숫자입력)
+    //0보다 작거나 6보다 크면,(guess값이 0 ~ 6 사이에 있는지 확인)
+    if (0 > guess || 6 < guess) {
 
-//0보다 작거나 6보다 크면,(guess값이 0 ~ 6 사이에 있는지 확인)
+        //올바른 방 번호를 입력하세요!
+        alert('올바른 방 번호를 입력하세요!');
+    }
 
-//올바른 방 번호를 입력하세요!
+    //추측한 횟수에 1을 더한다.
+    guesses = guesses + 1;        
 
-//추측한 횟수에 1을 더한다.
-        
-//1 또는 2, 3번과 일치하면
+    //1 또는 2, 3번과 일치하면 
+    if (guess == location1 || guess == location2 || guess == location3){
 
-//hit 증가.
+        //명중!
+        alert('명중!');
 
-//전함 침몰!
+        //hit 증가.
+        hits = hits + 1;
 
-//실패
+        if (hits === 3){
+
+            //전함 침몰!
+            isSunk = true;
+        }
+    } else {
+
+        //실패
+        alert('실패');
+    }
+}
 
 //n번 발사! 명중률 : n%
-
+//명중률 소수점이하 버림
+console.log(guesses + '번 발사! ' + ' 명중률 : ' + Math.floor(hits / guesses * 100) + '%');
