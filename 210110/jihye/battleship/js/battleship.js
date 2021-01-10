d4f1274e6acdd0f240ce3a08216c3e3a03e5f401
kimji9663 <rlawlgp9663@naver.com>
2021-01-10 11:18:18 +0900

    Headfirst JS - 8. Battleship Game

---
+++ 210110/jihye/battleship/js/battleship.js
@@ -0,0 +1,22 @@
+var view = {
+
+    // 문자열 메세지를 인자로 받아서 메세지 영역에 출력한다.
+    displayMessage: function(msg) {
+        // 코드
+    },
+
+    // 게임을 초기화, 모든 miss와 hit을 제거한다. 
+    newGame: function() {
+        // 코드
+    },
+
+    // 게임을 세팅, 전함의 위치를 랜덤 배치한다.
+    setGame: function() {
+        // 코드
+    },
+
+    // 성공, 전함 위치를 클릭 시, "명중!" 이라는 alert을 띄우고 게임을 종료한다.
+    success: function() {
+        // 코드
+    }
+}
