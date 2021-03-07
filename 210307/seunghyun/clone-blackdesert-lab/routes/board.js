fba538e224054fd4d280fba24308db56d90392a7
seunghyun <ddock4you@gmail.com>
2021-03-07 23:03:10 +0900

    - 검은사막 연구소 클론 프로젝트 시작

---
+++ 210307/seunghyun/clone-blackdesert-lab/routes/board.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/list', (req, res) => {
+    res.render('board/list');
+});
+
+router.get('/view', (req, res) => {
+    res.render('board/view');
+});
+
+router.get('/wirte', (req, res) => {
+    res.render('board/write');
+});
+
+module.exports = router;
