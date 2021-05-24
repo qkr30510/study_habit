cc50188aee441e87e00d65d1d2618d045872b780
kimji9663 <rlawlgp9663@naver.com>
2021-05-25 08:57:12 +0900

    HeadFirst 10 - 요금 지불한 승객 확인하는 함수

--- 210522/firstClass.js
+++ 210522/firstClass.js
@@ -6,5 +6,28 @@ var passengers = [ {name: "김함수", paid: true},
                    {name: "최호출", paid: true} ];
 
 function checkPaid(passengers) {
-    // 코드작성
+    for (var i = 0; i < passengers.length; i++){
+        if(!passengers[i].paid){
+            return false;
+        }
+    }
+    return true;
 }
+
+function checkNoFly(passengers){
+    for (var i = 0; i < passengers.length; i++){
+        if(onNoFlyList(passengers[i].name)){
+            return false;
+        }
+    }
+    return true;
+}
+
+function printPassengers(passengers){
+    for (var i = 0; i < passengers.length; i++){
+        console.log(passengers[i].name);
+    }
+    return true;
+}
+
+// 위와같이 for문이 반복되는 코드 리팩토링 해보자.
