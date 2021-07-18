8ff9baaced2abd18cd3b130aefbe888e8d34206a
seunghyun <ddock4you@gmail.com>
2021-07-18 23:10:39 +0900

    - 로그인 모듈 추가

---
+++ 210718/seunghyun/config/tables.sql
@@ -0,0 +1,9 @@
+ CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    birth DATE NOT NULL,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW()
+);
